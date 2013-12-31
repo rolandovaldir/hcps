0bbdd92de50278942bdbef3f538e565a4ca40b08
Rolando <valdir.cadimo@gmail.com>
2013-12-30 21:15:11 -0400

    En desarrollo, impresiones de autorizacion
    solo falta el logo de la CPS

---
+++ apps/hospitalario/modules/autorizaciones_autopsia/templates/_impresion_autorizacion.php
@@ -0,0 +1,62 @@
+<div class="autorizacion-head">
+    AUTORIZACIÓN LEGAL<br />
+    CON CONSENTIMIENTO INFORMADO
+</div>
+<br />
+<table>
+    <tbody>
+        <tr>
+            <td class="td-head-void"></td>
+            <td class="lbl-head-right">Nº Historia Clínica</td>
+            <td class="td-head"></td>
+            <td class="td-head-void"></td>
+            <td class="lbl-head-right">Nº Matrícula</td>
+            <td class="td-head"></td>
+        </tr>
+    </tbody>
+</table>
+<br />
+<div class="titulo-autorizacion">AUTORIZACIÓN PARA REALIZAR AUTOPSIA</div>
+<p>Los familiares del Asegurado y/o beneficiario del fallecido. Otorgamos
+ nuestro pleno consentimiento al personal médico autorizado de la Caja Petrolera
+ de Salud, para que realicen una autopsia (examen anatomopatológico) con fines de 
+ llegar a la causa (etiología) de la enfermedad que determino el fallecimiento de
+ nuestro familiar.
+</p>
+<table cellspacing="0" cellpadding="1">
+    <tbody>
+        <tr>
+            <td class="dato firma"></td>
+            <td class="void"></td>
+            <td class="dato firma"></td>
+        </tr>
+        <tr>
+            <td class="dato">Nombre del Paciente</td>
+            <td></td>
+            <td class="dato">Firma del Paciente</td>
+        </tr>
+        <tr>
+            <td>C.I._________________________</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="dato firma"></td>
+            <td class="void"></td>
+            <td class="dato firma"></td>
+        </tr>
+        <tr>
+            <td class="dato">Nombre del familiar</td>
+            <td></td>
+            <td class="dato">Firma del Familiar</td>
+        </tr>
+        <tr>
+            <td>C.I._________________________</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+
+<p>Fecha y Hora ___________________________________________________</p>
--- apps/hospitalario/modules/autorizaciones_diagnostico_tratamiento/actions/actions.class.php
+++ apps/hospitalario/modules/autorizaciones_diagnostico_tratamiento/actions/actions.class.php
@@ -30,4 +30,64 @@ class autorizaciones_diagnostico_tratamientoActions extends autoAutorizaciones_d
         $this->form->setDefault('internado_id',$request->getParameter('internado_id'));
         //var_dump($this->form->getDefault('file_internacion_id'));
     }
+    
+    public function executeExportPdf(sfWebRequest $request)
+    {
+        /*
+        //$user = $this->getUser();
+        //echo $id = $user->getAttribute('id');
+        $form = $this->getRoute()->getObject();
+        */
+        // estilos para este tipo de documento
+        $css = file_get_contents(sfConfig::get('sf_root_dir') . '/web/css/impresion_autorizacion_pdf.css');
+        $css = '<style>'.$css.'</style>';
+        
+        $html = $this->getPartial('autorizaciones_diagnostico_tratamiento/impresion_autorizacion', array());
+        
+        // PDF
+        $config = sfTCPDFPluginConfigHandler::loadConfig();
+                  sfTCPDFPluginConfigHandler::includeLangFile($this->getUser()->getCulture());
+
+        // pdf object, reescrito
+        $pdf = new sfTCPDF("P", PDF_UNIT, 'Letter', true, 'UTF-8');
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('CPS');
+        $pdf->SetTitle('CPS');
+        $pdf->SetSubject('Caja Petrolera de Salud');
+        $pdf->SetKeywords('TCPDF, PDF, impresion');
+        
+        // set default header data
+//        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+        // set header and footer fonts
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+        
+        //$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM); 
+        $pdf->SetAutoPageBreak(TRUE, 6);
+        
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(0);
+        
+        $pdf->SetDisplayMode('real','default');  
+        // quitar la linea del header
+        $pdf->setHeaderData('',0,'','',array(0,0,0), array(255,255,255) );
+        
+        $pdf->setPrintHeader(false);
+        $pdf->setPrintFooter(false);
+        
+        // set default monospaced font
+        $pdf->SetMargins(30, 8, 15);
+        $pdf->SetFont('dejavusans', '', 11, '', true);
+        
+        $pdf->AddPage();
+        $pdf->writeHTML($css.$html, true, false, false, false, '');
+
+        // Close and output PDF document
+        $pdf->Output('Formulario706.pdf', 'I');
+
+        // Stop symfony process
+        throw new sfStopException();
+    }
 }
