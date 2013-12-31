def436408674e08d60dda7195452e0187cd2302c
git <valdir.cadima@gmail.com>
2013-12-30 23:42:39 -0400

    pagina bienvenida en reportes y internados alta

--- apps/hospitalario/modules/reportes/templates/indexSuccess.php
+++ apps/hospitalario/modules/reportes/templates/indexSuccess.php
@@ -1,2 +1,7 @@
 <?php use_stylesheet('/sfDoctrinePlugin/css/global.css', 'first') ?> 
 <?php use_stylesheet('/sfDoctrinePlugin/css/default.css', 'first') ?> 
+
+<div class="tbl-bienvenida" style="margin-left: 15%;">
+    <center><img  src="/images/bienvenida.png"></center>
+    <h2 align="center">Bienvenido al sistema de hospitalizacion</h2>
+</div>
